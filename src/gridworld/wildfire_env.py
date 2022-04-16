4a887b649db340fb5a83e50cd72bda9184ad1f36
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-04-16 19:03:51 -0400

    add skeleton wildfire env

---
+++ src/gridworld/wildfire_env.py
@@ -0,0 +1,33 @@
+import gym
+from gym import spaces
+	
+
+
+# class CustomEnv(gym.Env):
+# 	"""Custom Environment that follows gym interface"""
+
+# 	def __init__(self, arg1, arg2, ...):
+# 		super(CustomEnv, self).__init__()
+# 		# Define action and observation space
+# 		# They must be gym.spaces objects
+# 		# Example when using discrete actions:
+# 		self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
+# 		# Example for using image as input (channel-first; channel-last also works):
+# 		self.observation_space = spaces.Box(low=0, high=255,
+# 											shape=(N_CHANNELS, HEIGHT, WIDTH), dtype=np.uint8)
+
+# 	def step(self, action):
+# 		pass
+# 		return observation, reward, done, info
+
+# 	def reset(self):
+# 		pass
+# 		return observation  # reward, done, info can't be included
+
+
+# 	def render(self, mode='human'):
+# 		pass
+
+
+# 	def close (self):
+# 		pass
