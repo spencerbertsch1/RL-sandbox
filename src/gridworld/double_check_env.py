b3dfde63d342ce6163013fa21e91bb3689426e56
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-04-21 16:21:24 -0400

    add new script to double check envs

---
+++ src/gridworld/double_check_env.py
@@ -0,0 +1,17 @@
+from stable_baselines3.common.env_checker import check_env
+from wildfire_env import WildFireEnv
+from test_env import TestEnv
+from portfolio_opt import PortfolioOptEnv
+
+env = TestEnv()
+episodes = 50
+
+for episode in range(episodes):
+    done = False
+    obs = env.reset()
+
+    while done is False:#not done:
+        random_action = env.action_space.sample()
+        print("action",random_action)
+        obs, reward, done, info = env.step(random_action)
+        print('reward',reward, 'done', done)
