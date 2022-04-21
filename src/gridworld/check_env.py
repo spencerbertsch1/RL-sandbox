8f440edbdbd60b541baa1849c2f5c8022f9da992
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-04-21 11:45:55 -0400

    add script to check custom envs

---
+++ src/gridworld/check_env.py
@@ -0,0 +1,10 @@
+from stable_baselines3.common.env_checker import check_env
+from wildfire_env import WildFireEnv
+from test_env import TestEnv
+from portfolio_opt import PortfolioOptEnv
+
+
+env = PortfolioOptEnv()
+# It will check your custom environment and output additional warnings if needed
+check_env(env)
+print('Passing')
