e769ce356cbb0a00822c1fb4725f70c98933e493
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-05-06 17:04:34 -0400

    update for server test

--- src/gridworld/env_learn.py
+++ src/gridworld/env_learn.py
@@ -19,8 +19,8 @@ env.reset()
 
 model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
 
-TIMESTEPS = 1000  # <-- should be at least 1000
-for i in range(100):
+TIMESTEPS = 10  # <-- should be at least 1000
+for i in range(3):
 	# train the model TIMESTEPS number of times before saving a copy of the new model to disk
 	model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO")
 	model.save(f"{models_dir}/{TIMESTEPS*i}")
