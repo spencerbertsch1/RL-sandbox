33e7a10de28184128c3a6d9c309138f985b0318c
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-05-12 12:33:45 -0400

    load new model

--- src/gridworld/model_loader.py
+++ src/gridworld/model_loader.py
@@ -17,7 +17,7 @@ def main(MODEL: str):
     models_path = f'{models_dir}/18000.zip'
 
     # FIXME - remove this hard coded path later
-    models_path = '/Users/spencerbertsch/Desktop/dev/RL-sandbox/src/gridworld/models/1652136471/249000.zip'
+    models_path = '/Users/spencerbertsch/Desktop/RL-sandbox/src/gridworld/models/1652294918/580000.zip'
 
     # load the model 
     model = PPO.load(models_path, env=env)
