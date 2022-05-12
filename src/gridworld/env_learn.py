5d2ee70fed31ced73fa6e2d759793c9f919c03e1
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-05-12 15:14:27 -0400

    add env 5

--- src/gridworld/env_learn.py
+++ src/gridworld/env_learn.py
@@ -37,6 +37,8 @@ def main(env_version):
         from wildfire_env_v3 import WildFireEnv
     elif env_version == 4:
         from wildfire_env_v4 import WildFireEnv
+    elif env_version == 5:
+        from wildfire_env_v5 import WildFireEnv
     else:
         raise Exception('Please pass an environment number represented above! (1, 2, 3, ...)')
 
