2c64698206da85e33938cbc3efcae9fb12d92171
Spencer Bertsch <spencerbertsch99@gmail.com>
2022-05-18 10:46:53 -0400

    update for new model run

--- src/gridworld/double_check_env.py
+++ src/gridworld/double_check_env.py
@@ -1,9 +1,9 @@
 from stable_baselines3.common.env_checker import check_env
-from wildfire_env_v10 import WildFireEnv
+from wildfire_env import WildFireEnv
 from test_env import TestEnv
 from portfolio_opt import PortfolioOptEnv
 
-env = WildFireEnv(TRAIN_MODE=False, SHOW_IMAGE_BACKGROUND=False, SHOW_BURNED_NODES=False)
+env = WildFireEnv(TRAIN_MODE=False, SHOW_IMAGE_BACKGROUND=False, SHOW_BURNED_NODES=False, BOARD_SIZE=20)
 episodes = 2
 
 for episode in range(episodes):
